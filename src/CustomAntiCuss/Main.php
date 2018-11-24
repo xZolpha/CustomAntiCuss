5bc018fe53e8b97a6f68f556da4ca56b2ab147c2
xZolpha <xzolpha@gmail.com>
2018-11-24 20:23:50 +1000

    Add files via upload

--- src/CustomAntiCuss/Main.php
+++ src/CustomAntiCuss/Main.php
@@ -11,11 +11,21 @@ namespace CustomAntiCuss\Main;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
 
-class Main{
+class Main extends PluginBase implements Loader{
+
+	public function onEnable(): void{
+		$this->getLogger()->info(TextFormat::GREEN . "CustomAntiCuss Enabled");
+	}
+
+	public function onDisable(): void{
+		$this->getLogger()->info(TextFormat::RED . "CustomAntiCuss Disabled");
+	}
 
 	private $anticuss = ["anal", "anus", "ass", "bastard", "bitch", "boob", "cock", "cum", "cunt", "dick", "dildo", "dyke", "fag", "faggot", "fuck", "fuk", "handjob", "homo", "jizz", "cunt", "kike", "kunt", "muff", "nigger", "penis", "piss", "poop", "pussy", "queer", "rape", "semen", "sex", "shit", "slut", "titties", "twat", "vagina", "vulva", "wank", "FUCK", "BITCH", "FAGGOT", "DICK", "CUNT", "ASS"];
-	public function onChat(PlayerChatEvent $event) : void{
+
+	public function onChat(PlayerChatEvent $event): void{
 		$msg = $event->getMessage();
 		$player = $event->getPlayer();
 		foreach($this->anticuss as $anticuss){
@@ -23,6 +33,7 @@ class Main{
 				$player->sendMessage(TextFormat::RED . "§l§fHey! §cYou §4cannot§c say that here! - §r§fPlugin made by Zolpha#0001");
 				$event->setCancelled();
 				return;
+				}
 			}
 		}
 	}
